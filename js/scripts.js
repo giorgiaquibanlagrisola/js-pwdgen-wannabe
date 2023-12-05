18d3ba1e77622bd4f588d3497d01450c06f84d45
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2023-12-05 17:10:25 +0100

    Nome

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
-// Test funzionamento
-console.log('Template HTML CSS JS');
+/*
+
+    1) Chiedi all'utente il suo nome,
+    2) poi chiedi il suo cognome,
+    3) poi chiedi il suo colore preferito
+    4) Infine scrivi sulla pagina nomecognomecolorepreferito114
+
+*/
+// 1) Chiedi all'utente il suo nome
+const userName = prompt('Dimmi il tuo nome');
+console.log('userName',userName,typeof userName);
+
